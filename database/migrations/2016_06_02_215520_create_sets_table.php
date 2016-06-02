ef511826e2ebe800f762a1cd99dd7726eae0fc8e
Dee Barizo <deebarizo@gmail.com>
2016-06-02 22:21:01 +0000

    Creates migration

---
+++ database/migrations/2016_06_02_215520_create_sets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sets', function($table)
+        {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('code')->unique();
+            $table->date('release_date');
+            $table->date('created_at');
+            $table->date('updated_at');
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sets');
+    }
+}
