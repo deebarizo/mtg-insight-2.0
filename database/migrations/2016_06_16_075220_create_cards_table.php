fc4a38ef0299bb813e4d3e6e1faaa59b0a9a83f2
Dee Barizo <deebarizo@gmail.com>
2016-06-16 11:15:05 +0000

    Adds nullable to fields in cards table

--- database/migrations/2016_06_16_075220_create_cards_table.php
+++ database/migrations/2016_06_16_075220_create_cards_table.php
@@ -16,16 +16,16 @@ class CreateCardsTable extends Migration
         {
             $table->increments('id');
             $table->string('name')->unique();
-            $table->string('color');
-            $table->string('mana_cost');
-            $table->integer('cmc');
+            $table->string('color')->nullable();
+            $table->string('mana_cost')->nullable();
+            $table->integer('cmc')->nullable();
             $table->string('type');
-            $table->text('rules_text');
-            $table->string('power');
-            $table->string('toughness');
-            $table->string('loyalty');
-            $table->string('f_cost');
-            $table->text('note');
+            $table->text('rules_text')->nullable();
+            $table->string('power')->nullable();
+            $table->string('toughness')->nullable();
+            $table->string('loyalty')->nullable();
+            $table->string('f_cost')->nullable();
+            $table->text('note')->nullable();
             $table->date('created_at');
             $table->date('updated_at');
         }); 
