df634714f748d7e08705a51158db6770413cc04e
Dee Barizo <deebarizo@gmail.com>
2016-09-18 01:56:21 +0000

    Makes rating column in cards table NOT nullable

---
+++ database/migrations/2016_09_18_015522_make_rating_column_in_cards_table_NOT_nullable.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeRatingColumnInCardsTableNOTNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cards', function ($table) {
+            
+            $table->integer('rating')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cards', function ($table) {
+            
+            $table->integer('rating')->nullable()->change();
+        });
+    }
+}
